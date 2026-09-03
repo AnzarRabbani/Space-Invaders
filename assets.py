3d7c8ee89178664e64bfb0adff5757ba4c946a79
AnzarRabbani <anzarrabbani6311@gmail.com>
2026-09-03 09:07:59 +0400

    Final Version

--- assets.py
+++ assets.py
@@ -2,46 +2,52 @@ import pygame;pygame.init()
 
 BLACK='#000000'
 WHITE='#FFFFFF'
+GREEN='#26FF1F'
+RED='#FF1F26'
 
 WIDTH,HEIGHT=800,800
 
 WIN=pygame.display.set_mode((WIDTH,HEIGHT))
 
 playerspeed=4
-enemyspeed=2
+enemyspeed=5
 
 laserspeed=6
 
 playerhealth=5
-enemyhealth=1
 
 playercooldown=250#ms
 enemycooldown=500#ms
 
 enemydirection=1
 
-BACKGROUND=pygame.image.load('assets/background.png').convert_alpha()
+score=0
+
+with open('highscore.txt','r') as f:
+	highscore=int(f.read())
+
+BACKGROUND=pygame.image.load('Assets/background.png').convert_alpha()
 BACKGROUND=pygame.transform.scale(BACKGROUND,(WIDTH,HEIGHT))
 
-PLAYER=pygame.image.load('assets/player.png').convert_alpha()
+PLAYER=pygame.image.load('Assets/player.png').convert_alpha()
 PLAYER=pygame.transform.scale(PLAYER,(50,50))
 
-ENEMY1=pygame.image.load('assets/enemy1.png').convert_alpha()
+ENEMY1=pygame.image.load('Assets/enemy1.png').convert_alpha()
 ENEMY1=pygame.transform.scale(ENEMY1,(50,50))
 
-ENEMY2=pygame.image.load('assets/enemy2.png').convert_alpha()
+ENEMY2=pygame.image.load('Assets/enemy2.png').convert_alpha()
 ENEMY2=pygame.transform.scale(ENEMY2,(50,50))
 
-ENEMY3=pygame.image.load('assets/enemy3.png').convert_alpha()
+ENEMY3=pygame.image.load('Assets/enemy3.png').convert_alpha()
 ENEMY3=pygame.transform.scale(ENEMY3,(50,50))
 
-ENEMY4=pygame.image.load('assets/enemy4.png').convert_alpha()
+ENEMY4=pygame.image.load('Assets/enemy4.png').convert_alpha()
 ENEMY4=pygame.transform.scale(ENEMY4,(50,50))
 
-LASER1=pygame.image.load('assets/playerlaser.png').convert_alpha()
+LASER1=pygame.image.load('Assets/playerlaser.png').convert_alpha()
 #LASER1=pygame.transform.scale(LASER1,(WIDTH,HEIGHT))
 
-LASER2=pygame.image.load('assets/enemylaser.png').convert_alpha()
+LASER2=pygame.image.load('Assets/enemylaser.png').convert_alpha()
 #LASER2=pygame.transform.scale(LASER2,(WIDTH,HEIGHT))
 
 class Ship:
@@ -57,10 +63,10 @@ class Ship:
 		self.laserspeed=laserspeed
 	def draw(self):
 		WIN.blit(self.img,self.rect)
-	def move(self):
-		self.rect.x+=self.speed*enemydirection
+	def move(self,direction):
+		self.rect.x+=self.speed*direction
 	def movedown(self):
-		self.rect.y+=self.speed
+		self.rect.y+=1
 	def shoot(self):
 		WIN.blit(self.laserimg,self.laser)
 
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from assets import *
 
 pygame.display.set_caption('Space Invaders')
 clock=pygame.time.Clock()
+FONT1=pygame.font.SysFont('Comic Sans MS',30)
+FONT3=pygame.font.SysFont('Comic Sans MS',50)
+FONT2=pygame.font.SysFont('Comic Sans MS',100)
 
 player=Ship(WIDTH/2,700,PLAYER,playerspeed,playerhealth,LASER1,laserspeed)
 
@@ -14,12 +17,7 @@ playerlasers=[]
 lastshottime=0
 
 enemylist=[]
-
-def enemygroupsize():
-	groupsize=random.randint(1,10)
-	return groupsize
-
-groupsize=1
+enemylasers=[]
 
 def enemydesign():
 	number=random.randint(1,4)
@@ -33,61 +31,182 @@ def enemydesign():
 		design=ENEMY4
 	return design
 
-design=enemydesign()
+
 
 padding=30
 spacing=ENEMY1.get_width()+padding
+enemiesdead=True
 
-for x in range(int(WIDTH/spacing)):
-	for y in range(groupsize):
-		enemylist.append(Ship((x*spacing)+40,(y*50)+50,design,enemyspeed,1,LASER2,laserspeed))
+decending=False
+decendingLeft=0
+
+def enemygroupsize():
+	groupsize=random.randint(1,8)
+	return groupsize
+
+def draw_enemies():
+	global enemiesdead;enemiesdead=False
+	global design;design=enemydesign()
+	global groupsize;groupsize=enemygroupsize()
+	for x in range(int((WIDTH/spacing)-2)):
+		for y in range(groupsize):
+			enemylist.append(Ship((x*spacing)+50,(y*-80)-40,design,enemyspeed,1,LASER2,laserspeed))
+
+GameOverText=FONT2.render('Game Over',False,WHITE)
+RestartText=FONT3.render('Press r to restart or q to quit',False,WHITE)
+
+state='playing'
 
 run=True
 while run:
-	clock.tick(60)
-	WIN.fill(BLACK)
-	WIN.blit(BACKGROUND,(0,0))
 
-	player.draw()
-			
-	for enemy in enemylist:
-		enemy.draw()
+	if state=='playing':
+		clock.tick(60)
+		WIN.fill(BLACK)
+		WIN.blit(BACKGROUND,(0,0))
 
-	for enemy in enemylist:
-		if enemy.rect.center[0]<=40:
+		scoreText=FONT1.render('Score: '+str(score),False,WHITE)
+		highscoreText=FONT1.render('Highscore: '+str(highscore),False,WHITE)
+
+		WIN.blit(scoreText,(20,750))
+		WIN.blit(highscoreText,(20,770))
+
+		player.draw()
+
+		greenHealthRect=pygame.Rect(player.rect.x,player.rect.y+player.rect.height,playerhealth*10,10)
+		redHealthRect=pygame.Rect(player.rect.x+greenHealthRect.width,player.rect.y+player.rect.height,(5-playerhealth)*10,10)
+		pygame.draw.rect(WIN,GREEN,greenHealthRect)
+		pygame.draw.rect(WIN,RED,redHealthRect)
+
+		if enemiesdead:
+			draw_enemies()
+			print(groupsize)
+		
+		if len(enemylist)==0:
+			enemiesdead=True
+			score+=50
+
+		for enemy in enemylist:
+			enemy.draw()
+
+		hitedge=False
+		for enemy in enemylist:
+			if enemy.rect.center[0]<=40 or enemy.rect.center[0]>=WIDTH-40:
+				hitedge=True
+				break
+		
+		if hitedge:
 			enemydirection*=-1
-			enemy.movedown()
-			break
-		elif enemy.rect.center[0]>=WIDTH-40:
-			enemydirection*=-1
-			enemy.movedown()
-			break
-	
-	for enemy in enemylist:
-		enemy.move()
+			decending=True
+			decendingLeft=40
 
-	for event in pygame.event.get():
-		if event.type==pygame.QUIT:
-			run=False
-		if event.type==pygame.KEYDOWN:
-			if event.key==pygame.K_q:
+		if decending:
+			decendingLeft-=1
+			for enemy in enemylist:
+				enemy.movedown()
+			if decendingLeft<=0:
+				decending=False
+
+		for enemy in enemylist:
+			enemy.move(enemydirection)
+
+		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
 				run=False
-			if event.key==pygame.K_SPACE:	
-				if (pygame.time.get_ticks()-lastshottime)>playercooldown:
-					playerlasers.append(Laser(player.rect.center[0],player.rect.center[1],LASER1,laserspeed))
-					lastshottime=pygame.time.get_ticks()
-				
-	for laser in playerlasers:
-		laser.draw()
-		laser.move()
+			if event.type==pygame.KEYDOWN:
+				if event.key==pygame.K_q:
+					run=False
+				if event.key==pygame.K_SPACE:	
+					if (pygame.time.get_ticks()-lastshottime)>playercooldown:
+						playerlasers.append(Laser(player.rect.center[0],player.rect.center[1],LASER1,laserspeed))
+						lastshottime=pygame.time.get_ticks()
+					
+		for laser in playerlasers:
+			laser.draw()
+			laser.move()
+		
+		shot_enemy=None
+		laser_shot=None
+		for laser in playerlasers:
+			for enemy in enemylist:
+				if laser.rect.colliderect(enemy):
+					shot_enemy=enemy
+					laser_shot=laser
+					score+=10
+					break
+		
+		playershot=None
+		for laser in enemylasers:
+			if laser.rect.colliderect(player.rect):
+				playershot=laser
+				break
 
+		playertouch=False
+		for enemy in enemylist:
+			if enemy.rect.center[1]+(1.5*ENEMY1.get_height())>=player.rect.y+player.rect.height:
+				playertouch=True
+				break
+		
+		if playertouch:
+			playerhealth=0
 
-	keys=pygame.key.get_pressed()
-	if keys[pygame.K_LEFT]:
-		player.moveleft()
-	if keys[pygame.K_RIGHT]:
-		player.moveright()
+		if playershot:
+			enemylasers.remove(playershot)
+			playerhealth-=1
+			print(playerhealth)
 
-	pygame.display.update()
+		if playerhealth<=0:
+			if score>highscore:
+				highscore=score
+				with open('highscore.txt','w') as f:
+					f.write(str(score))
+			state='gameover'
+
+		if shot_enemy:
+			enemylist.remove(shot_enemy)
+			playerlasers.remove(laser_shot)
+
+		def willEnemyshoot():
+			number=random.randint(1,1000)
+			if number==1:
+				return True
+
+		for enemy in enemylist:
+			if willEnemyshoot():
+				enemylasers.append(Laser(enemy.rect.center[0],enemy.rect.center[1],LASER2,laserspeed*-1))
+		
+		for laser in enemylasers:
+			laser.draw()
+			laser.move()
+
+		keys=pygame.key.get_pressed()
+		if keys[pygame.K_LEFT] and player.rect.center[0]>=(player.rect.width/2):
+			player.move(-1)
+		if keys[pygame.K_RIGHT] and player.rect.center[0]<=(WIDTH-(player.rect.width/2)):
+			player.move(1)
+
+		pygame.display.update()
+	
+	elif state=='gameover':
+		clock.tick(60)
+		WIN.blit(GameOverText,((WIDTH/2)-(GameOverText.get_width()/2),(HEIGHT/2)-(GameOverText.get_height()/2)))
+		WIN.blit(RestartText,((WIDTH/2)-(RestartText.get_width()/2),(HEIGHT/2)-(RestartText.get_height()/2)+50))
+
+		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
+				run=False
+
+			if event.type==pygame.KEYDOWN:
+				if event.key==pygame.K_q:
+					run=False
+				if event.key==pygame.K_r:
+					score=-50
+					playerhealth=5
+					state='playing'
+					enemylist=[]
+					playerlasers=[]
+					enemylasers=[]
+		pygame.display.update()
+		
 
 pygame.quit()
